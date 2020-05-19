378684a0000eaf626d35a0585e1ab565ecae40eb
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-19 23:09:13 +0530

    Added Daily Wages Using Dictionary

--- assignment/day9/empWageComputation.sh
+++ assignment/day9/empWageComputation.sh
@@ -91,15 +91,16 @@ echo "Calculating For a Month"
 
 
 #-------------New Branch------------------------------------------
-	IS_PART_TIME=1;
-	IS_FULL_TIME=2;
-	MAX_HRS_IN_MONTH=4;
-	EMP_RATE_PER_HR=20;
-	NUM_WORKING_DAYS=20;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-	totalEmpHr=0;
-	totalWorkingDays=0;
-	while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 	do
    	((totalWorkingDays++))
    	empCheck=$((RANDOM%3));
@@ -112,12 +113,13 @@ echo "Calculating For a Month"
 				;;
 			*)
 				empHrs=0
+
 				;;
 		esac
 	totalEmpHrs=$(($totalEmpHrs+$empHrs))
 	done
 	totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
-	echo $totslSalary
+	echo $totalSalary
 #--------------------NewBranch--------------------------------------
 
 function getWorkHrs() {
@@ -138,5 +140,57 @@ function getWorkHrs() {
 function getEmpWage() {
    echo $(($1*$EMP_RATE_PER_HR));
 }
+
+#----------------New Branch---------------
+echo "Storing Daily Wages"
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHr=0;
+totalWorkingDays=0;
+
+declare -A dailyWages
+function getWorkHrs() {
+
+   case $1 in
+      $IS_FULL_TIME)
+         empHrs=8
+         ;;
+      $IS_PART_TIME)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+   esac
+   echo $empHrs;
+}
+
+function getEmpWage() {
+   echo $(($1*$EMP_RATE_PER_HR));
+}
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+   ((totalWorkingDays++))
+   empCheck=$((RANDOM%3));
+   empHrs="$(getWorkHrs $empCheck)"
+   totalEmpHrs=$(($totalEmpHrs+$empHrs))
+   dailyWages[$totalWorkingDays]="$( getEmpWage $empHrs )"
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+
+echo ${dailyWages[@]}
+
+
+
+
+
+
+
 
 
