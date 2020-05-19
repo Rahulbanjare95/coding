4c4fdba90206df30a2383a4066c26fd31c316670
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-19 15:19:53 +0530

    Attendence Checker Added

--- assignment/day9/empWageComputation.sh
+++ assignment/day9/empWageComputation.sh
@@ -1,1 +1,13 @@
 echo "Welcome to Employee Wage Computation Program"
+
+echo "Attendence check"
+
+	Present=1
+	attendence_checker=$((RANDOM%2))
+	if [ $Present -eq $attendence_checker ]
+	then
+		echo "Employee Is Present"
+	else
+		echo "Employee is Absent"
+	fi
+
