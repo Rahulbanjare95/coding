143edc5eea6b37cdd542a658e39a3d0c20949b8e
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-19 16:29:53 +0530

    Adding Part time feature

--- assignment/day9/empWageComputation.sh
+++ assignment/day9/empWageComputation.sh
@@ -23,3 +23,24 @@ echo "Calculating Daily Wage"
 		dailyWage=0
 	fi
 	echo "Daily Wage = "$dailyWage
+
+echo "Adding Part Time Work Hours"
+
+	isPartTime=1
+	isFullTime=2
+	empRatePerHr=20
+
+	work_hrs_checker=$((RANDOM%3))
+	if [ $isFullTime -eq $work_hrs_checker ]
+	then
+		empWorkHrs=8;
+		echo "Full-Time"
+	elif [ $isPartTime -eq $work_hrs_checker ]
+	then
+		empWorkHrs=4;
+		echo "Part-Time"
+	else
+		empWorkHrs=0;
+	fi
+	salary=$(( $empWorkHrs * $empRatePerHr ))
+	echo "Salary= " $salary
