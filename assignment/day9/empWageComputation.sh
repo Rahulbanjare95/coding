0f349c50a2c7f244f4ce2d3d3db63d060daa4879
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-19 18:14:36 +0530

    Added Maximum Work Hrs Feature

--- assignment/day9/empWageComputation.sh
+++ assignment/day9/empWageComputation.sh
@@ -88,3 +88,33 @@ echo "Calculating For a Month"
 		TotalSalary=$(($TotalSalary+$salary))
 	done
 	echo "Salay of month: " $TotalSalary
+
+
+#-------------New Branch------------------------------------------
+	IS_PART_TIME=1;
+	IS_FULL_TIME=2;
+	MAX_HRS_IN_MONTH=4;
+	EMP_RATE_PER_HR=20;
+	NUM_WORKING_DAYS=20;
+
+	totalEmpHr=0;
+	totalWorkingDays=0;
+	while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+	do
+   	((totalWorkingDays++))
+   	empCheck=$((RANDOM%3));
+		case $empCheck in
+			$IS_FULL_TIME)
+				empHrs=8
+				;;
+			$IS_PART_TIME)
+				empHrs=4
+				;;
+			*)
+				empHrs=0
+				;;
+		esac
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	done
+	totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+	echo $totslSalary
